502cce9b629edde27641f61a4c2d1ab388812acd
Nikita <twimar2@gmail.com>
2016-12-10 14:50:06 +0300

    Add files via upload

---
+++ zadach_2_var4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cout<<"Vvedite Dvuxznachnoe chislo ";
+	cin>> a;
+	b=a/10;
+	cout<<"Kol-vo desatkov = "<<b<<endl;
+	c=a-(b*10);
+	cout<<"Ostatok = "<<c<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ zadacha_1_var4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	float r,l,s;
+	const float pi= 3.14;
+	cout<<"Vvedite r= ";
+	cin >>r;
+	l=2*pi*r;
+	cout <<"Dlina = "<<l<<endl;
+	s=pi*r*r;
+	cout <<"S= "<<s<<endl;
+	
+	
+	return 0;
+}
